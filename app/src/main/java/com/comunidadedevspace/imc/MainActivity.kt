f138b72f9f7587c8948f347bc4cca936ea9b6527
ivancorse <ivancorse91@gmail.com>
2024-11-19 01:37:07 +0000

    Melhorando experiencia do usuário (layout)

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -38,19 +38,11 @@ class MainActivity : AppCompatActivity() {
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
 
-                // Navegar para proxima tela
-                // Criar o layout da proxima tela
-                // Passara dados para proxima tela
 
-                // Intent - Classe do proprio android
-
-                val intent = Intent(this, ResultActivity::class.java )
+                val intent = Intent(this, ResultActivity::class.java)
                 intent.putExtra(KEY_RESULT_IMC, resultado)
                 startActivity(intent)
 
-                // Cores
-                // EditText background + Icone
-                // Gradiente + icone + titulo + descrição
 
             }
 
